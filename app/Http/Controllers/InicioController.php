86a2e9084e8d4092268d13b92d2e0ebdf5d844de
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-08-19 17:30:58 -0500

    feat: create a seeder for products

--- app/Http/Controllers/InicioController.php
+++ app/Http/Controllers/InicioController.php
@@ -12,7 +12,7 @@ class InicioController extends Controller
     public function index()
     {
         //obtener las productos mas recientes
-        $nuevos = Product::orderBy('created_at', 'Desc')->paginate(9);
+        $nuevos = Product::orderBy('created_at', 'Desc')->paginate(3);
         //nuevos = Product::latest()->take(10)->get();
 
         return view('inicio.index', compact('nuevos'));
