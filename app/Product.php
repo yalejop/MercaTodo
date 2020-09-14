f4a2511d40f9ef349e0a5ae4e6f3fb2b1bc6ae1c
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-09-14 11:50:52 -0500

    feat: create pivot tables and belongs to many relations

--- app/Product.php
+++ app/Product.php
@@ -2,16 +2,29 @@
 
 namespace App;
 
+use App\Cart;
+use App\Order;
+use App\CategoriaProducto;
 use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
     protected $fillable = [
-        'title', 'description', 'price', 'stock', 'image', 'categoria_id',
+        'title', 'description', 'price', 'stock', 'image', 'categoria_id', 'status',
     ];
 
     public function categoria()
     {
         return $this->belongsTo(CategoriaProducto::class);
+    }
+
+    public function carts()
+    {
+        return $this->belongsToMany(Cart::class)->withPivot('quantity');
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class)->withPivot('quantity');
     }
 }
---
+++ database/migrations/2020_09_14_163431_create_order_product_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrderProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_product', function (Blueprint $table) {
+            $table->foreignId('order_id')->references('id')->on('orders');
+            $table->foreignId('product_id')->references('id')->on('products');
+            $table->integer('quantity')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_product');
+    }
+}
