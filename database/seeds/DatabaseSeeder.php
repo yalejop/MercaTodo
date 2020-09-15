adb9b70bfafd12ce9afe025e0e6eecaa24e4cf94
Alejandro Cataño Herrera <alejandrocatanoherrera@gmail.com>
2020-09-14 22:39:19 -0500

    feat: insert seeders for polimorphs relations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Cart;
+use App\User;
+use App\Order;
+use App\Payment;
 use App\Product;
 use Illuminate\Database\Seeder;
 
@@ -16,6 +20,36 @@ class DatabaseSeeder extends Seeder
         $this->call(PermissionInfoSeeder::class);
         $this->call(CategoriasSeeder::class);
 
-        $products = factory(Product::class, 15)->create();
+        $users = factory(User::class, 20)->create();
+
+        $orders = factory(Order::class, 10)
+            ->make()
+            ->each(function ($order) use ($users) {
+                $order->user_id = $users->random()->id;
+                $order->save();
+
+                $payment = factory(Payment::class)->make();
+                // $payment->order_id = $order->id;
+                // $payment->save();
+                $order->payment()->save($payment);
+            });
+
+            $carts = factory(Cart::class, 20)->create();
+
+            $products = factory(Product::class, 50)
+                ->create()
+                ->each(function ($product) use ($orders, $carts) {
+                    $order = $orders->random();
+    
+                    $order->products()->attach([
+                        $product->id => ['quantity' => mt_rand(1, 3)],
+                    ]);
+    
+                    $cart = $carts->random();
+    
+                    $cart->products()->attach([
+                        $product->id => ['quantity' => mt_rand(1, 5)],
+                    ]);
+                });
     }
 }
